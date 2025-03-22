e28879ee8b7c733ebfa034946814ca11b577d646
Jaspe Michael <micaeljaspe@gmail.com>
2025-03-22 19:14:20 +0200

    feat: added test case for the LDA Zero Page Indexed mode

--- src/main.c
+++ src/main.c
@@ -5,8 +5,10 @@ int main(){
 	CPU *cpu = malloc(sizeof(CPU));
 	Memory *mem = malloc(sizeof(Memory));
 	reset(cpu, mem);
-	mem->data[cpu->pc] = INS_LDA_IM;
-	mem->data[(cpu->pc)+1] = 0b01000000;
-	execute(cpu, mem, CCL_LD_IM);
+	mem->data[cpu->pc] = INS_LDA_ZPX;
+	mem->data[(cpu->pc)+1] = 0x80;
+	cpu->x = 0x0F;
+	mem->data[0x8F] = 0xDE;
+	execute(cpu, mem, CCL_LD_ZPX);
 	return 0;
 }
