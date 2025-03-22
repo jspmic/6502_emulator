733723aec121bc0affca4919f7e0b685b83bde33
Jaspe Michael <micaeljaspe@gmail.com>
2025-03-22 19:16:06 +0200

    feat: added procedures to read without the PC and handles for the LDA, LDX, and LDY

--- src/proc.c
+++ src/proc.c
@@ -22,31 +22,59 @@ Byte fetch(u32 *cycles, CPU* cpu, Memory* mem){
 	return data;
 }
 
+Byte read_without_pc(u32 *cycles, Byte address, Memory* mem){
+	Byte data = mem->data[address];
+
+	(*cycles)--;
+	return data;
+}
+
+void LDSet(CPU* cpu){
+	cpu->Z = (cpu->a) == 0;
+	cpu->N = ((cpu->a) & 0b01000000) > 0;
+}
+
 void execute(CPU* cpu, Memory* mem, u32 cycles){
 	for (;cycles > 0;){
 		Byte instruction = fetch(&cycles, cpu, mem);
-		Byte value;
+		Byte operand, addr_value;
 		switch (instruction){
 			case INS_LDA_IM:
-				value = fetch(&cycles, cpu, mem);
-				cpu->a = value;
-				cpu->Z = (cpu->a) == 0;
-				cpu->N = ((cpu->a) & 0b01000000) > 0;
+				operand = fetch(&cycles, cpu, mem);
+				cpu->a = operand;
+				LDSet(cpu);
 				break;
 			case INS_LDX_IM:
-				value = fetch(&cycles, cpu, mem);
-				cpu->x = value;
+				operand = fetch(&cycles, cpu, mem);
+				cpu->x = operand;
 				cpu->Z = (cpu->x) == 0;
 				cpu->N = ((cpu->x) & 0b01000000) > 0;
 				break;
 			case INS_LDY_IM:
-				value = fetch(&cycles, cpu, mem);
-				cpu->y = value;
+				operand = fetch(&cycles, cpu, mem);
+				cpu->y = operand;
 				cpu->Z = (cpu->y) == 0;
 				cpu->N = ((cpu->y) & 0b01000000) > 0;
 				break;
+			case INS_LDA_ZP:
+				operand = fetch(&cycles, cpu, mem); // Zero Page address
+				addr_value = read_without_pc(&cycles, operand, mem);
+				cpu->a = addr_value;
+				LDSet(cpu);
+				break;
+			case INS_LDA_ZPX:
+				operand = fetch(&cycles, cpu, mem); // Zero Page address
+
+				// If the sum exceeds 1 byte, it will truncate it
+				operand += (cpu->x);
+				cycles--;
+
+				addr_value = read_without_pc(&cycles, operand, mem);
+				cpu->a = addr_value;
+				LDSet(cpu);
+				break;
 			default:
-				printf("Default case: %d\n", instruction);
+				printf("Unexceptional case not handled: %d\n", instruction);
 				break;
 		}
 	}
