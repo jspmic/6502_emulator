071c688256f3d0c8abb74b7408de6fef62257a11
Jaspe Michael <micaeljaspe@gmail.com>
2025-03-22 17:36:34 +0200

    feat: added test case to check if the struct members are structured after writing to and reading from memory

--- src/main.c
+++ src/main.c
@@ -5,5 +5,8 @@ int main(){
 	CPU *cpu = malloc(sizeof(CPU));
 	Memory *mem = malloc(sizeof(Memory));
 	reset(cpu, mem);
+	mem->data[cpu->pc] = INS_LDA_IM;
+	mem->data[(cpu->pc)+1] = 0b01000000;
+	execute(cpu, mem, CCL_LD_IM);
 	return 0;
 }
