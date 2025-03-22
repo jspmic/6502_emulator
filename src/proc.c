91b003ca5075e05ff07a1828773acddf29222121
Jaspe Michael <micaeljaspe@gmail.com>
2025-03-22 10:26:24 +0200

    fix: fixed the reset procedure by 0-ing the memory using memset not calloc

--- src/proc.c
+++ src/proc.c
@@ -1,10 +1,11 @@
 #include "header.h"
-#include <stdlib.h>
+#include <memory.h>
 
+/* Resets the CPU and Memory according to 6502 indications */
 void reset(CPU* cpu, Memory* mem){
 	cpu->pc = 0xFFFC;
 	cpu->sp = 0x00;
 	cpu->D = 0;
 	cpu->I = 0;
-	mem = calloc(MEM, sizeof(Byte));
+	memset(mem->data, 0, sizeof(mem->data));
 }
