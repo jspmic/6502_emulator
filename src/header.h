9850fbca1828a0fb1d614d8ccf8c5c418d204889
Jaspe Michael <micaeljaspe@gmail.com>
2025-04-05 12:14:08 +0200

    feat: added functions needed by instructions.c in this header file
    - added also the macro NO_INSTRUCTIONS

--- src/header.h
+++ src/header.h
@@ -2,6 +2,7 @@
 #define GUARD
 
 #define MEM 1024*64
+#define NO_INSTRUCTIONS 256
 
 typedef unsigned char Byte;
 typedef unsigned short Word;
@@ -57,7 +58,25 @@ enum CYCLES{
 	CCL_JSR			= 6,		// Cycle for the JSR instruction
 };
 
+typedef void (*f_instruction)(u32*, CPU*, Memory*);
+
+typedef struct{
+	f_instruction functions[NO_INSTRUCTIONS];
+	Byte init: 1;
+} function_manager;
+
+// Functions provided by instructions.c
+void init(void);
+void execute_instruction(Byte opcode, u32* cycles, CPU* cpu, Memory* mem);
+
+// Functions provided by proc.c
+void LDSet(CPU* cpu);
+Byte read_without_pc(u32 *cycles, Word address, Memory* mem);
 void reset(CPU* cpu, Memory* mem);
+void free_resource(CPU** cpu, Memory** mem);
+Byte fetch_byte(u32 *cycles, CPU* cpu, Memory* mem);
+Word fetch_word(u32 *cycles, CPU* cpu, Memory* mem);
+void write_word(Word value, u32 addr, u32 *cycles, Memory* mem);
 void execute(CPU* cpu, Memory* mem, u32 cycles);
 void free_resource(CPU** cpu, Memory** mem);
 
