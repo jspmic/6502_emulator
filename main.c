0ead62c5ee1185291f4fcfe34b156f2d325be63e
Jaspe Michael <micaeljaspe@gmail.com>
2026-08-07 22:30:47 +0200

    feat: added detailed comments on how the program test1.a65 works

--- main.c
+++ main.c
@@ -9,6 +9,12 @@ int main(int argc, char** argv){
 	reset(cpu, mem);
 
 	// Loader code
+	// File code expected result:
+	// [0x200] <- 0xA1
+	// [0x201] <- 0xF2
+	// [0x202] <- 0xCA
+	// **** tested ****
+
 	char* name = "./examples/a.o65";
 	FILE* file = read_binary(name);
 	load_memory(file, mem, cpu);
